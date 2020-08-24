d39a7a7e55a2f32854e6fa2f90f7cfd06107a9d3
Jakkapan <tatarksb@windowslive.com>
2020-08-25 00:22:35 +0700

    Updates

--- precache-manifest.4899c881a3ceb149760c248329996e60.js
+++ precache-manifest.4899c881a3ceb149760c248329996e60.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18182d7c3d43544e017087f9b0b54b7e",
+    "revision": "c91f577a6ce19dda511cee3c1fb629e4",
     "url": "/index.html"
   },
   {
-    "revision": "65fb412d1732eefeb6c2",
+    "revision": "17b238f18de8dc06b0c9",
     "url": "/static/css/main.27f0fd95.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f1f33499.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "65fb412d1732eefeb6c2",
-    "url": "/static/js/main.f063405d.chunk.js"
+    "revision": "17b238f18de8dc06b0c9",
+    "url": "/static/js/main.01faf338.chunk.js"
   },
   {
     "revision": "7d44e6a5cb781c7eb69f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac6c7c552ff3a9256970fda246a39544.js"
+  "/precache-manifest.4899c881a3ceb149760c248329996e60.js"
 );
 
 self.addEventListener('message', (event) => {
