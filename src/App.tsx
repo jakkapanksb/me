262d6f9cd4d91623c839b7926f802a29b335a1be
Jakkapan <tatarksb@windowslive.com>
2020-09-09 20:57:44 +0700

    Preload images

--- src/App.tsx
+++ src/App.tsx
@@ -62,6 +62,18 @@ const NavTime = withNav(Time);
 const NavAnxious = withNav(Anxious);
 const NavAbout = withNav(About);
 
+const cacheImages = (srcArray: string[]) => {
+  srcArray.forEach((src) => {
+    new Promise(function (resolve, reject) {
+      const img = new Image();
+
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    });
+  });
+};
+
 const App = () => {
   const [state, setState] = React.useState({ step: 0 });
   const [showCursor, setShowCursor] = React.useState(true);
@@ -71,6 +83,33 @@ const App = () => {
   const stepIncrement = React.useCallback(() => {
     setState((state) => ({ ...state, step: state.step + 1 }));
   }, [setState]);
+
+  React.useEffect(() => {
+    const imgs = [
+      // Disconnected
+      'https://www.dropbox.com/s/hlvvtza3894b9z1/Disconnected%2001.jpg?raw=1',
+      'https://www.dropbox.com/s/kj931zhnfyvcnad/Disconnected%2002.jpg?raw=1',
+      'https://www.dropbox.com/s/b6q8hlvo548r4y8/Disconnected%2006.png?raw=1',
+      // 'https://www.dropbox.com/s/u9dlkh6q7iu77fo/Disconnected%2006_hover.png?raw=1',
+      // 'https://www.dropbox.com/s/qtcnrxf1ix9p5r6/Disconnected%2003_hover.jpg?raw=1',
+      // 'https://www.dropbox.com/s/iffl5v4bhng3vzs/Disconnected%2003.png?raw=1',
+      // 'https://www.dropbox.com/s/kiaucokh13nnhom/Disconnected%2005.jpg?raw=1',
+      // 'https://www.dropbox.com/s/lunv6jrrqxyd09i/Disconnected%2005_hover.jpg?raw=1',
+      // 'https://www.dropbox.com/s/f4ud04587hro140/Disconnected%2007.png?raw=1',
+      // 'https://www.dropbox.com/s/zxn0nr7k06ceyqj/Disconnected%2007_hover.jpg?raw=1',
+
+      // Time
+      'https://www.dropbox.com/s/f4rnkswrwhlwpo0/Time%2001.jpg?raw=1',
+      'https://www.dropbox.com/s/uwx6wlrg46pz9o9/Time%2003.jpg?raw=1',
+      'https://www.dropbox.com/s/i6elpc554esb5h0/Time%2004.jpg?raw=1',
+
+      // Anxiety
+      'https://www.dropbox.com/s/y3cp4d1gvf1es33/Anxious%2001.jpg?raw=1',
+      'https://www.dropbox.com/s/pevtu8xjils69hq/Anxious%2002.jpg?raw=1',
+    ];
+
+    cacheImages(imgs);
+  }, []);
 
   return (
     <MainLayout id='main-layout' pathname={pathname}>
